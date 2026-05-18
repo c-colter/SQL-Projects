3b2a8daaed6f8cf499f10a8b0cb49eb3427ff191
chance <86379472+c-colter@users.noreply.github.com>
2026-05-18 13:58:52 -0400

    Create ex_2.sql

---
+++ cte-exercises/ex_2.sql
@@ -0,0 +1,15 @@
+-- Select supporters who donated more than $200 total or who donated at least twice.
+WITH over_200 AS (
+	SELECT supporter_id FROM donation
+	GROUP BY supporter_id
+	HAVING SUM(amount) > 200
+),
+multiple_donations AS (
+	SELECT supporter_id FROM donation
+	GROUP BY supporter_id
+	HAVING COUNT(*) > 2
+)
+SELECT first_name || ' ' || last_name FROM supporter
+WHERE id IN (
+	SELECT supporter_id FROM over_200 INTERSECT SELECT supporter_id FROM multiple_donations
+);
